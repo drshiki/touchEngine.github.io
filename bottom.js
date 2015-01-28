6e76ce0b4ccba2d57af4691746feb554157e053f
Dam H Jame <jianmaster@iCloud.com>
2015-01-28 15:33:49 +0800

    55

--- bottom.js
+++ bottom.js
@@ -1,5 +1,21 @@
 document.write("\
 </div>\
+<center>\
+<div  id = \"comment_area_2\">\
+<div id=\"disqus_thread\"></div>\
+<script type=\"text/javascript\">\
+var disqus_shortname = 'damhjame'; // required: replace example with your forum shortname\
+(function() {\
+var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;\
+dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';\
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);\
+})();\
+</script>\
+<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>\
+</div>\
+</center>\
+</div>\
+</div>\
 <div id = \"foot\">\
 <p id = \"foot_p\">\
 Designed by Dam H Jame,Base on Github Pages<br/>\
--- top.js
+++ top.js
@@ -6,6 +6,18 @@ document.write("\
 <link rel = stylesheet type = \"text/css\" href = \"index.css\"/>\
 </head>\
 <body id = \"body_style\">\
+<div id = \"first_containter\">\
+<div id = \"comment_area_1\">\
+<script type=\"text/javascript\">\
+var disqus_shortname = 'damhjame'; // required: replace example with your forum shortname\
+(function () {\
+var s = document.createElement('script'); s.async = true;\
+s.type = 'text/javascript';\
+s.src = '//' + disqus_shortname + '.disqus.com/count.js';\
+(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);\
+}());\
+</script>\
+</div>\
 <h1 id = \"head_pic\"> </h1>\
 <input id = \"search_box\" class = \"search_box\" type = \"text\" name = \"search_box\" maxlength = 255>\
 <input id = \"search_button\" class = \"search_button\" type = \"button\" name = \"search_button\" value = \"search\" onclick = google_search(\"search_box\")>\
