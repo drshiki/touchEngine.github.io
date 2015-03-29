a404123aaf05f31de81c04f08aae73911b0dc75f
Dam <jianmaster@icloud.com>
2015-03-29 10:19:12 +0800

    publish an essay

--- top.js
+++ top.js
@@ -7,17 +7,8 @@ document.write("\
 </head>\
 <body id = \"body_style\">\
 <div id = \"first_containter\">\
-<div id = \"comment_area_1\">\
-<script type=\"text/javascript\">\
-var disqus_shortname = 'damhjame'; // required: replace example with your forum shortname\
-(function () {\
-var s = document.createElement('script'); s.async = true;\
-s.type = 'text/javascript';\
-s.src = '//' + disqus_shortname + '.disqus.com/count.js';\
-(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);\
-}());\
-</script>\
 </div>\
+<div id = \"comment_area_1\">\
 <h1 id = \"head_pic\"> </h1>\
 <input id = \"search_box\" class = \"search_box\" type = \"text\" name = \"search_box\" maxlength = 255>\
 <input id = \"search_button\" class = \"search_button\" type = \"button\" name = \"search_button\" value = \"search\" onclick = google_search(\"search_box\")>\
